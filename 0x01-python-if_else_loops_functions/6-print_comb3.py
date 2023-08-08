4d3c925f6f45a92faafb31ab40151c97e8e80b40
Abel-mwirigi <abelmwirigi632@gmail.com>
2023-08-08 23:11:10 +0300

    commit to add python files

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# run iterations from 0 to 99
+# print the iteration number
+# 
+
+for i in range(0, 10):
+	for j in range(i + 1, 10):
+		if i == 8 and j == 9:
+			print("{:d}{:d}".format(i, j))
+		else:
+			print("{:d}{:d}, ".format(i, j), end="")
