9a252dca65ed5bca803139f3e3835711347c7614
Abel-mwirigi <abelmwirigi632@gmail.com>
2023-08-15 08:41:44 +0300

    a program that prints #pythoniscool, followed by a new line, in the standard output.

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from for_python_is_cool import python_is_cool
-    python_is_cool()
+from for_python_is_cool import python_is_cool
+python_is_cool()
