f814c031be7af4a6f09225be189866a6074c766a
Abel-mwirigi <abelmwirigi632@gmail.com>
2023-08-28 21:23:45 +0300

    a function that raises a name exception with a message

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result = []
-    
     for i in range(list_length):
         try:
             value_1 = my_list_1[i]
             value_2 = my_list_2[i]
-            
-            if type(value_1) not in (int, float) or type(value_2) not in (int, float):
+            if type(value_1) not in (int, float):
+                result.append(0)
+                print("wrong type")
+            elif type(value_2) not in (int, float):
                 result.append(0)
                 print("wrong type")
             elif value_2 == 0:
@@ -21,6 +22,4 @@ def list_division(my_list_1, my_list_2, list_length):
         except Exception as e:
             result.append(0)
             print(e)
-    
     return result
-
